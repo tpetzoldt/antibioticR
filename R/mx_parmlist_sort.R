48cfe34694f5fd0d2e852ae312abe93a0cbb29aa
thpe <thomas.petzoldt@tu-dresden.de>
2018-10-19 11:01:01 +0200

    add mx_select and mx_subset

--- R/mx_parmlist_sort.R
+++ R/mx_parmlist_sort.R
@@ -1,9 +1,10 @@
 #
 #' Sort and Select Distribution Components
 #'
-#' @param p   mixture distribution parameter list
+#' @param p mixture distribution object or parameter list
 #' @param N number of components
 #' @param item parameter name used for sorting and selection (e.g. 'L', 'mean', 'sd')
+#' @param type kind of subset
 #' @param ascending ascending  (TRUE) ode descending (FALSE) order
 #' @param as.vector if TRUE, result is returned as a vector, otherwise as list
 #' @param ... reserved for future extensions
@@ -109,3 +110,90 @@ mx_wildtype <- function(p, as.vector = TRUE) {
   if (as.vector) main_comp <- unlist(main_comp[-1], recursive = FALSE)
   main_comp
 }
+
+#' @rdname mx_parmlist_sort
+#'
+#' @export
+#'
+mx_select <- function(obj, type=c("resistant", "wildtype", "intermediate",
+                                  "hypersensitive", "nonresistant")) {
+
+  type <- match.arg(type)
+
+  if(isClass("mxObj", obj)) p <- coef(obj)
+
+  if (type=="wildtype") {
+    ## main normal component with largest L
+    ## if no mean exists (e.g. exponential), set it to zero
+    ndx <- which.max(sapply(p, function(x) ifelse(x$type == "e", 0, x$L)))
+
+  } else if (type == "resistant") {
+    ndx <- which(sapply(p, function(x) x$type == "e"))
+
+  } else if (type=="nonresistant") {
+    ndx <- which(sapply(p, function(x) x$type != "e"))
+
+  } else if (type=="hypersensitive") {
+    wt <- mx_select(obj, "wildtype")
+    mean_wt <- p[[wt]]$mean
+
+    ndx <- which(sapply(p, function(x) {
+      mean_x <- ifelse(x$type == "e", 0, x$mean) # set mean of exponential to zero
+      mean_x > mean_wt}
+    ))
+
+  } else if (type=="intermediate") {
+    wt <- mx_select(obj, "wildtype")
+    mean_wt <- p[[wt]]$mean
+    ndx <- which(sapply(p, function(x) {
+      mean_x <- ifelse(x$type == "e", 0, x$mean)
+      (mean_x > 0) & (mean_x < mean_wt)}
+    ))
+  }
+
+  if (length(ndx) == 0) ndx <- NULL
+
+  ndx
+}
+
+#' @rdname mx_parmlist_sort
+#'
+#' @export
+#'
+mx_subset <- function(p, type=c("resistant", "wildtype", "intermediate",
+                                "hypersensitive", "nonresistant"), as.vector = TRUE) {
+
+  ndx <- mx_select(p, type=type)
+
+  if(isClass("mxObj", p)) p <- coef(p)
+
+  if (is.null(ndx)) {
+    component <- NULL
+  } else {
+    component <- p[ndx]
+    ## strip list node if only one component
+    if (length(component) == 1) {
+      nested_list <- FALSE
+      component <- p[[ndx]]
+    } else {
+      nested_list <- TRUE
+    }
+    if (as.vector) {
+      # strip non-numeric type element from list
+      if (nested_list) {
+        tmp <- lapply(component, function(x) {
+          x$type <- NULL
+          x
+        })
+      } else {
+        tmp <- component[-1] # except "type" element
+      }
+      component <- unlist(tmp)
+      ## if > 1 component, substitute n1.sd, n1.mean with sd1, mean2, ...
+      names(component) <- sub("^([a-z])([0-9]+)([.])(.+)", "\\4\\2", names(component))
+    }
+  }
+  component
+}
+
+
